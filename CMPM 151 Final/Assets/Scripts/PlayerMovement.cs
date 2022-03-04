33072e3dd9b1d217ea1c54c47fc840ac26d16ecf
James Doherty <starmagic1999@gmail.com>
2022-03-04 14:10:41 -0800

    Basic OSC setup

--- CMPM 151 Final/Assets/Scripts/PlayerMovement.cs
+++ CMPM 151 Final/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// unityOSC namespace
+using UnityOSC;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.runInBackground = true;
+
+        OSCHandler.Instance.Init();
+    }
+
+    void FixedUpdate() {
+
+		OSCHandler.Instance.UpdateLogs();
+		Dictionary<string, ServerLog> servers = new Dictionary<string, ServerLog>();
+		servers = OSCHandler.Instance.Servers;
+
+		foreach (KeyValuePair<string, ServerLog> item in servers) {
+			// If we have received at least one packet,
+			// show the last received from the log in the Debug console
+			if (item.Value.log.Count > 0) {
+				int lastPacketIndex = item.Value.packets.Count - 1;
+
+				//get address and data packet
+				countText.text = item.Value.packets [lastPacketIndex].Address.ToString ();
+				countText.text += item.Value.packets [lastPacketIndex].Data [0].ToString ();
+
+			}
+		}
     }
 
     // Update is called once per frame
