84ed7030621ccbadaf03e1bf9d609723dc753530
Chantel <chmagee@ucsc.edu>
2022-03-03 18:18:13 -0800

    player movement

--- CMPM 151 Final/Assets/Scripts/PlayerMovement.cs
+++ CMPM 151 Final/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+    public Animator animator;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection = 0f;
+
+    // Awake is called after all objects are initialized. Called in random order
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // will look for a component on this GameObject (what the script is attached to) of type Rigidbody2D
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        processInputs();
+
+        // Animate
+        animate();
+
+        // Move
+        move();
+
+    }
+
+    private void move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (Input.GetKeyDown("w"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+        }
+    }
+
+    private void animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            flipChara();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            flipChara();
+        }
+    }
+
+    private void processInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); // from -1 to 1
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection));
+    }
+
+    private void flipChara()
+    {
+        facingRight = !facingRight; //inverse boolean
+        transform.Rotate(0f, 180f, 0f);
     }
 }
